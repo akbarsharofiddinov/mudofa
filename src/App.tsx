a8b45346dae3574adb616e465b2c15c1503b7a81
akbarovshoxruz <proteach025@gmail.com>
2025-10-13 15:40:17 +0500

    Add search functionality with image display: Implement SearchInput and PhotoRoom components, integrate Pixabay API, and style with SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Achievements, Entrance, Home, Management, OurTeam } from "@/pages"
 import ErrorPageHandling from './pages/ErrorPageHandling/ErrorPageHandling'
+import SearchInput from './pages/Search/SearchInput'
+import PhotoRoom from './pages/Search/PhotoRoom'
 
 const App: React.FC = () => {
+
+  const [data, setData] = useState<{ imageData: string[] }>({ imageData: [] })
+
+  const setImageData = (data: string[]) => {
+    setData({ imageData: data })
+  }
 
   const routes = createBrowserRouter([
     {
@@ -41,10 +49,15 @@ const App: React.FC = () => {
 
   return (
     <>
-      <RouterProvider router={routes} />
+      {/* <RouterProvider router={routes} /> */}
       {/* <div className='w-full h-screen'>
         <HorseModel />
       </div> */}
+      <div className="w-full h-screen flex flex-col items-center overflow-hidden">
+        <h1 className='text-center font-bold text-2xl m-2'>React Photo Room</h1>
+        <SearchInput setImageData={setImageData} />
+        <PhotoRoom imageData={data.imageData}></PhotoRoom>
+      </div>
     </>
   )
 }
